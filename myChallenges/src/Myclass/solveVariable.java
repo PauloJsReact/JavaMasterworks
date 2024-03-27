616f822de1fd4e73b6cdffde7e252d816a5b6987
Paulo Araujo <paulo_henrique_araujo@outlook.com>
2024-03-27 10:43:54 -0300

    easy problem

---
+++ myChallenges/src/Myclass/solveVariable.java
@@ -0,0 +1,21 @@
+package Myclass;
+
+public class solveVariable {
+
+	int a = 10;
+	static int f = 11; 
+	public static void main(String[] args) {
+		int b = new solveVariable().a;
+		int c = new solveVariable().a;
+		
+		solveVariable d = new solveVariable();
+		
+		
+		System.out.println(b);
+		System.out.println(c-5);
+		System.out.println(d.a);
+		System.out.println(f);
+		
+	
+	}
+}
